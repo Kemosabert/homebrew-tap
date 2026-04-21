b7125716a1b121041b749292aaa1eb8da57bd6f7
Kemosabert <bert@aikido.dev>
2026-04-21 13:53:53 +0200

    update sha

--- Formula/jsed.rb
+++ Formula/jsed.rb
@@ -7,7 +7,7 @@ class Jsed < Formula
   on_macos do
     on_arm do
       url "https://github.com/Kemosabert/jsed/releases/download/v0.0.2/jsed-v0.0.2-macos-aarch64.tar.gz"
-      sha256 "sha256:af9a710bd4bc7c6a173f91573ab9082552fbda5b3ed91b17ed6b3cafb22bba22"
+      sha256 "af9a710bd4bc7c6a173f91573ab9082552fbda5b3ed91b17ed6b3cafb22bba22"
     end
   end
 
