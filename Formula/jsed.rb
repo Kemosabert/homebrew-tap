224a39e84d4fda2675fa2bbc8511e1a5683f3a60
Kemosabert <bert@aikido.dev>
2026-04-21 13:51:25 +0200

    update version of json editor

--- Formula/jsed.rb
+++ Formula/jsed.rb
@@ -1,13 +1,13 @@
 class Jsed < Formula
   desc "Super tiny JSON editor for big files"
   homepage "https://github.com/Kemosabert/jsed"
-  version "0.0.1"
+  version "0.0.2"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/Kemosabert/jsed/releases/download/v0.0.1/jsed-v0.0.1-macos-aarch64.tar.gz"
-      sha256 "44225562c81c09c38f27ff147623817d483a49a7ab19757d5a2d7cb77316bfdb"
+      url "https://github.com/Kemosabert/jsed/releases/download/v0.0.2/jsed-v0.0.2-macos-aarch64.tar.gz"
+      sha256 "sha256:af9a710bd4bc7c6a173f91573ab9082552fbda5b3ed91b17ed6b3cafb22bba22"
     end
   end
 
